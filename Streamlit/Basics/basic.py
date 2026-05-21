bc310fef971f0f1a1285caffcbcb20d1868d9bee
Om Patil <ompatilm4@gmail.com>
2026-05-21 17:12:27 +0530

    Done with Streamlit Web Framework

---
+++ Streamlit/Basics/basic.py
@@ -0,0 +1,9 @@
+import streamlit as st 
+
+st.title("My First Streamlit App")
+
+st.header("This is a header")
+st.subheader("This is a subheader") 
+
+st.text("This is a text")
+st.slider("This is a slider", 0,100,50) 
